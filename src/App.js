97cde82d23ed02c3fc1de63f3d7080f2cee0ad08
liuyib <1656081615@qq.com>
2021-04-21 18:14:56 +0800

    chore: install antd & config router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <div>hello td sheep village tools</div>
-}
+import React from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import Home from './pages/Home/index'
+import NotFound from './pages/404'
+import 'antd/dist/antd.css'
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/404">
+          <NotFound />
+        </Route>
+        <Route path="*">
+          <Redirect to="/404" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
